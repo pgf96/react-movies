1425cac0163e3fbeca8d85519844ec9d13446d33
pgf96 <pgfxx1@gmail.com>
2023-02-06 00:01:17 -0500

    refactor actor list

--- src/pages/ActorListPage/ActorListPage.jsx
+++ src/pages/ActorListPage/ActorListPage.jsx
@@ -2,12 +2,12 @@ import ActorCard from '../../components/ActorCard/ActorCard'
 import  Row  from "react-bootstrap/Row"
 import  Col  from "react-bootstrap/Col"
 
-const ActorListPage = ({moviesList, user}) => {
+const ActorListPage = ({moviesList}) => {
   const actorsList = moviesList.reduce((cast, movie) => 
     [...cast, ...movie.cast],[]
   )
+
   const actors = Array.from(new Set(actorsList))
-  console.log(actors)
 
   return (
     
